b9dbd5ca9b59aa840a00513801ef4a2634b9e08d
angryhamburger <brian@thecorp.org>
2017-01-31 16:59:24 -0500

    Implementing python into Website

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Post
 from django.utils import timezone
 
 def post_list(request):
-	 posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {})
 
 # Create your views here.
